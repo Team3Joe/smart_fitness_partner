43a711cd3bd2258c5371935e7a0470ddfe019ea3
youngjin <daphnejin36@hanmail.net>
2022-06-17 14:09:05 +0900

    06171408
    메인과 합침

---
+++ korean_fitness/lib/message5.dart
@@ -0,0 +1,13 @@
+class exercise {
+  String exName;
+  String imagePath;
+  String exCount;
+
+  exercise(
+      {
+        required this.exName,
+        required this.imagePath,
+        required this.exCount,
+      }
+  );
+}
